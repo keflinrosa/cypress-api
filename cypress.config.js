3e9aacc60f0316a2fcd319fe1dfe6432b3e6ee83
keflinrosa <keflinduarte@gmail.com>
2022-08-09 00:06:06 +0100

    add a GET test

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   e2e: {
+    baseUrl: 'http://todo-app-barkend.herokuapp.com/',
+    specPattern: 'cypress/integration/api-tests',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      baseUrl: 'http://todo-app-barkend.herokuapp.com/'
+      
     },
   },
 };
