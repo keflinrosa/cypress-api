0b8dc6d6641f1f03618bb85ee540b4bca594575f
keflinrosa <keflinduarte@gmail.com>
2022-08-09 23:22:53 +0100

    Updating

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,9 @@ module.exports = {
   projectId: '2k5pc1',
   e2e: {
     baseUrl: 'http://todo-app-barkend.herokuapp.com/',
-    specPattern: 'cypress/integration/api-tests',
+    specPattern: 'cypress/e2e',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
   },
 };
